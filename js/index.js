204c25bb5923cfa98deabde66e818b8e0165992a
MatheusOliveira047 <ludsonmatheus@gmail.com>
2022-01-05 00:28:17 -0300

    meu primeiro commit

---
+++ js/index.js
@@ -0,0 +1,14 @@
+//const scrollreveal = require("./scrollreveal");
+
+window.sr = ScrollReveal();
+    sr.reveal('.container',{
+        duration:3000,
+        origin:'bottom',
+        distance:'-100px'
+    });
+  
+    sr.reveal('.about-scroll',{
+        duration:3000,
+        origin:'bottom',
+        distance:'-100px'
+    });
