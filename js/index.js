458c97b0f5fee730dcf2cf4b561a800b46701a75
MatheusOliveira047 <ludsonmatheus@gmail.com>
2022-01-05 11:27:53 -0300

    outras coisas

--- js/index.js
+++ js/index.js
@@ -7,8 +7,40 @@ window.sr = ScrollReveal();
         distance:'-100px'
     });
   
+    sr.reveal('.box1',{
+        duration:3000,
+        origin:'bottom',
+        distance:'-100px'
+    });
+    sr.reveal('.box2',{
+        duration:3500,
+        origin:'bottom',
+        distance:'-100px'
+    });
+    sr.reveal('.box3',{
+        duration:4000,
+        origin:'bottom',
+        distance:'-100px'
+    });
+    sr.reveal('.box4',{
+        duration:4500,
+        origin:'bottom',
+        distance:'-100px'
+    });
+    sr.reveal('.box5',{
+        duration:5000,
+        origin:'bottom',
+        distance:'-100px'
+    });
+    sr.reveal('.box6',{
+        duration:5500,
+        origin:'bottom',
+        distance:'-100px'
+    });
+  
     sr.reveal('.dev',{
         delay:1000,
         duration:3000,
         distance:'-10px'
     });
+    
