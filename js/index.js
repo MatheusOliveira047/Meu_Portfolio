b9a958a0a9cd65a4036081fa46dfb77feca1cbad
MatheusOliveira047 <ludsonmatheus@gmail.com>
2022-01-06 22:11:20 -0300

    scroll

--- js/index.js
+++ js/index.js
@@ -6,41 +6,35 @@ window.sr = ScrollReveal();
         origin:'bottom',
         distance:'-100px'
     });
-  
-    sr.reveal('.box1',{
-        duration:3000,
-        origin:'bottom',
-        distance:'-100px'
-    });
-    sr.reveal('.box2',{
-        duration:3500,
-        origin:'bottom',
-        distance:'-100px'
-    });
-    sr.reveal('.box3',{
-        duration:4000,
-        origin:'bottom',
-        distance:'-100px'
-    });
-    sr.reveal('.box4',{
-        duration:4500,
-        origin:'bottom',
-        distance:'-100px'
-    });
-    sr.reveal('.box5',{
-        duration:5000,
-        origin:'bottom',
-        distance:'-100px'
-    });
-    sr.reveal('.box6',{
-        duration:5500,
-        origin:'bottom',
-        distance:'-100px'
-    });
-  
+ 
     sr.reveal('.dev',{
         delay:1000,
         duration:3000,
+        distance:'-10px'
+    });
+    sr.reveal('.you-about',{
+        origin:'bottom',
+        duration:2500,
+        distance:'-10px'
+    });
+    sr.reveal('.p1',{
+        origin:'bottom',
+        duration:3000,
+        distance:'-10px'
+    });
+    sr.reveal('.p2',{
+        origin:'bottom',
+        duration:3500,
+        distance:'-10px'
+    });
+    sr.reveal('.p3',{
+        origin:'bottom',
+        duration:4000,
+        distance:'-10px'
+    });
+    sr.reveal('.p4',{
+        origin:'bottom',
+        duration:4500,
         distance:'-10px'
     });
     
