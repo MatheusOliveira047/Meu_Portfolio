e8b8eee2ecc4710056c0c6307f43ffc13d808ad1
MatheusOliveira047 <ludsonmatheus@gmail.com>
2022-04-11 14:46:02 -0300

    atualizações do menu responsivo

--- js/mobile-navbar.js
+++ js/mobile-navbar.js
@@ -1,42 +1,10 @@
-class MobileNavbar{
-    constructor(mobileMenu, navList, navLinks){
-        this.mobileMenu = document.querySelector(mobileMenu);
-        this.navList = document.querySelector(navList);
-        this.navLinks = document.querySelectorAll(navLinks);
-        this.activeClass = "active"
-
-        this.handleClick = this.handleClick.bind(this)
-    }
-    animateLinks(){
-        this.navLinks.forEach((link, index) => {
-            link.style.animation
-            ? (link.style.animation = "")
-            : (link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`);
-        });
-    }
-
-    handleClick(){
-
-        this.navList.classList.toggle(this.activeClass);
-        this.mobileMenu.classList.toggle(this.activeClass)
-        this.animateLinks();
-    }
-
-    addClickEvent(){
-        this.mobileMenu.addEventListener("click", this.handleClick);
-    }
-
-    init(){
-        if (this.mobileMenu){
-            this.addClickEvent();
-        }
-        return this;
-    }
+function menu(){
+    let header = document.querySelector(".header")
+    let me = document.querySelector(".menu")
+    let fechar = document.querySelectorAll(".nav-list li a")
+    fechar.forEach((a)=>{
+        a.classList.toggle("active")
+    })
+    me.classList.toggle("active")
+    header.classList.toggle("active")
 }
-
-const mobileNavbar = new MobileNavbar (
-    ".mobile-menu",
-    ".nav-list",
-    ".nav-list li",
-);
-mobileNavbar.init();
